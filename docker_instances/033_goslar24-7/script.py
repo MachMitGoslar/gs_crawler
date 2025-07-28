8e20a253abcffe6c3376bd2cf3088daa37e25d58
Janssen, Nantke <nantke.janssen@goslar.de>
2025-07-28 09:18:16 +0200

    Neuer Service: Goslar 24/7

---
+++ docker_instances/033_goslar24-7/script.py
@@ -0,0 +1,23 @@
+import logging
+import schedule
+import time
+from get_and_store_images import fetch_and_store_images, delete_old_images
+from create_gif import create_gif_from_db_images
+
+# define logging
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
+def job():
+    fetch_and_store_images()
+    create_gif_from_db_images()
+    delete_old_images()
+
+schedule.every().hour.at(":00").do(job)
+job()
+logging.info("⏳ Warte auf nächste Ausführung ...")
+while True:
+    schedule.run_pending()
+    time.sleep(60)
