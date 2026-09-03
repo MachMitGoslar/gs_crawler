ba940e1708e47eaec3e6b59d705a68af96cfe072
rangarius <stuff@stuffdev.de>
2026-09-03 20:46:54 +0200

    removed console logs

--- docker_instances/019_was_app/019_was_app.py
+++ docker_instances/019_was_app/019_was_app.py
@@ -18,11 +18,8 @@ soup = BeautifulSoup(response.text, "html.parser")
 
 timeline_container = soup.find("div", class_="project-step-timeline-container")
 headline = timeline_container.find_next("h4", class_="project-step-timeline-title") if timeline_container  else None
-print(headline.get_text(strip=True) if headline else None)
 description_element_wrapper = timeline_container.find_next("div", class_="project-step-timeline-text") if timeline_container else None
-print(description_element_wrapper.get_text(strip=True) if description_element_wrapper else None)
 description_element = description_element_wrapper.find_next("p") if description_element_wrapper else None
-print(description_element.get_text(strip=True) if description_element else None)
 image_div = soup.find("div", class_="c-hero")
 image_url = image_div.find("img")["src"] if image_div and image_div.find("img") else "" 
 jetzt = datetime.now()
